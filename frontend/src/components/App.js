1e44869f0adfb53d6c773e7aaf81ae9c96d43a6f
alevanpraag <alevanpraag@gmail.com>
2023-12-20 15:40:06 -0600

    center everything

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,7 +4,7 @@ import HomePage from './HomePage';
 
 export default function App(props) {
     return (
-         <div>
+         <div className="center">
             <HomePage/>
         </div>
     );
